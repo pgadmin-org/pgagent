ea61b139c94f188f6bc48777843f44c287e32460
Dave Page <dpage@pgadmin.org>
2012-01-12 10:43:14 +0000

    Copyright updates

--- include/connection.h
+++ include/connection.h
@@ -2,7 +2,7 @@
 //
 // pgAgent - PostgreSQL Tools
 //
-// Copyright (C) 2002 - 2011, The pgAdmin Development Team
+// Copyright (C) 2002 - 2012, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
 // connection.h - database connection
