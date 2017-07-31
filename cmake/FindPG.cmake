a7b82fb3d3bd3de3ce624d9e4b0da53cb9835c2d
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2017-07-31 14:57:55 +0530

    Added support two digits version for PostgreSQL/EDB Postgres Advanced Server >= 10

--- cmake/FindPG.cmake
+++ cmake/FindPG.cmake
@@ -28,7 +28,6 @@
 # PG_VERSION_STRING - The PostgreSQL version number.
 # PG_MAJOR_VERSION - The PostgreSQL major version (x in x.y.z).
 # PG_MINOR_VERSION - The PostgreSQL minor version (y in x.y.z).
-# PG_PATCH_VERSION - The PostgreSQL patch version (z in x.y.z).
 # PG_EXTENSION - Set to TRUE if PostgreSQL supports extensions.
 
 IF(NOT PG_STATIC OR PG_STATIC STREQUAL "")
@@ -70,8 +69,9 @@ IF(NOT _retval)
     # Split the version into its component parts.
     STRING(REGEX MATCHALL "[0-9]+" PG_VERSION_PARTS "${PG_VERSION_STRING}")
     LIST(GET PG_VERSION_PARTS 0 PG_MAJOR_VERSION)
-    LIST(GET PG_VERSION_PARTS 1 PG_MINOR_VERSION)
-    LIST(GET PG_VERSION_PARTS 2 PG_PATCH_VERSION)
+    IF((PG_MAJOR_VERSION LESS 10))
+        LIST(GET PG_VERSION_PARTS 1 PG_MINOR_VERSION)
+    ENDIF((PG_MAJOR_VERSION LESS 10))
 
     # Are extensions supported?
     IF((PG_MAJOR_VERSION GREATER 9) OR ((PG_MAJOR_VERSION EQUAL 9) AND (PG_MINOR_VERSION GREATER 0)))
