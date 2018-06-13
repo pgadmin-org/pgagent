dae1a10bfb4368c8510bce5b02d07d1b15934d53
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2018-06-13 13:39:56 +0100

    Rename the "from unpackaged" extension SQL script and bump it's schema version.

--- sql/pgagent--unpackaged--4.0.sql
+++ sql/pgagent--unpackaged--4.0.sql
@@ -1,6 +1,6 @@
 /*
 // pgAgent - PostgreSQL Tools
-// 
+//
 // Copyright (C) 2002 - 2018 The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
@@ -42,3 +42,11 @@ SELECT pg_catalog.pg_extension_config_dump('pga_schedule', '');
 SELECT pg_catalog.pg_extension_config_dump('pga_exception', '');
 SELECT pg_catalog.pg_extension_config_dump('pga_joblog', '');
 SELECT pg_catalog.pg_extension_config_dump('pga_jobsteplog', '');
+
+CREATE OR REPLACE FUNCTION pgagent.pgagent_schema_version() RETURNS int2 AS '
+BEGIN
+    -- RETURNS PGAGENT MAJOR VERSION
+    -- WE WILL CHANGE THE MAJOR VERSION, ONLY IF THERE IS A SCHEMA CHANGE
+    RETURN 4;
+END;
+' LANGUAGE 'plpgsql' VOLATILE;
